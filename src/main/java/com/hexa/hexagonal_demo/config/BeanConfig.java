7fac68f2b3bd85e009d8f6e66755e9783f945098
getrojob <getrojob@gmail.com>
2026-04-01 13:55:50 -0300

    refactor BeanConfig to simplify RestClient usage

--- src/main/java/com/hexa/hexagonal_demo/config/BeanConfig.java
+++ src/main/java/com/hexa/hexagonal_demo/config/BeanConfig.java
@@ -2,6 +2,7 @@ package com.hexa.hexagonal_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 import com.hexa.hexagonal_demo.application.service.CreateUserService;
 import com.hexa.hexagonal_demo.application.service.GetAddressService;
@@ -39,8 +40,8 @@ public class BeanConfig {
     }
 
     @Bean
-    public org.springframework.web.client.RestClient.Builder restClientBuilder() {
-        return org.springframework.web.client.RestClient.builder();
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 }
 
